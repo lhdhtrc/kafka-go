77f41a20c7a49c6bfa3a125f9d1d2f72799875e6
lhdht <15229684090@163.com>
2024-06-09 09:37:28 +0800

    feature: 数据模型

--- model/tls.go
+++ model/tls.go
@@ -4,5 +4,4 @@ type TLSEntity struct {
 	CaCert        string `json:"ca_cert" bson:"ca_cert" yaml:"ca_cert" mapstructure:"ca_cert"`
 	ClientCert    string `json:"client_cert" bson:"client_cert" yaml:"client_cert" mapstructure:"client_cert"`
 	ClientCertKey string `json:"client_cert_key" bson:"client_cert_key" yaml:"client_cert_key" mapstructure:"client_cert_key"`
-	ClusterCert   string `json:"cluster_cert" bson:"cluster_cert" yaml:"cluster_cert" mapstructure:"cluster_cert"`
 }
