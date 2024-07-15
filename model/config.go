40d6e13ccd03e49de4e9ae3bdf322399f9850e33
lhdht <15229684090@163.com>
2024-07-15 15:10:59 +0800

    fix: 迁移

--- model/config.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type ConfigEntity struct {
-	Tls      TLSEntity `json:"tls" bson:"tls" yaml:"tls" mapstructure:"tls"`
-	Account  string    `json:"account" bson:"account" yaml:"account" mapstructure:"account"`
-	Password string    `json:"password" bson:"password" yaml:"password" mapstructure:"password"`
-	Address  []string  `json:"address" yaml:"address" mapstructure:"address"`
-}
